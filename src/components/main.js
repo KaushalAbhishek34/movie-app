d7441986c12c39fbb0e35eb02d24bad6487f75ef
abhishek kaushal <abhikaushalak17@gmail.com>
2024-01-16 10:31:41 +0530

    456372

--- src/components/main.js
+++ src/components/main.js
@@ -8,14 +8,23 @@ const MovieApp = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   const [favorites, setFavorites] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
   const [showFavorites, setShowFavorites] = useState(false);
 
   useEffect(() => {
-    fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=cd1fa48b9c76b58e20e48ca5597505d7")
+    fetchMovies(currentPage);
+  }, [currentPage]);
+
+  const fetchMovies = (page) => {
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=cd1fa48b9c76b58e20e48ca5597505d7&page=${page}`)
       .then((response) => response.json())
-      .then((data) => setMovies(data.results))
+      .then((data) => {
+        setMovies(data.results);
+        setTotalPages(data.total_pages);
+      })
       .catch((error) => console.error('Error fetching data:', error));
-  }, []);
+  };
 
   const filteredMovies = movies.filter(movie =>
     movie.title.toLowerCase().includes(searchTerm.toLowerCase())
@@ -35,6 +44,12 @@ const MovieApp = () => {
     setShowFavorites(!showFavorites);
   };
 
+  const handlePagination = (newPage) => {
+    if (newPage >= 1 && newPage <= totalPages) {
+      setCurrentPage(newPage);
+    }
+  };
+
   return (
     <Container maxWidth="lg" sx={{ marginTop: 4 }}>
       <TextField
@@ -49,10 +64,10 @@ const MovieApp = () => {
       <Button variant="contained" color={showFavorites ? "primary" : "error"} onClick={handleShowFavorites} sx={{ marginLeft: 2 }}>
         {showFavorites ? 'All Movies' : ' Favorites'}
       </Button>
-      <Grid container spacing={1}>
+      <Grid container spacing={6}>
         {showFavorites
           ? favorites.map((movie) => (
-            <Grid item key={movie.id} s={12} md={6} lg={4}>
+            <Grid item key={movie.id} xs={12} sm={6} md={4}>
               <Card sx={{ height: "100%"  }}>
                 <CardMedia
                   component="img"
@@ -104,9 +119,9 @@ const MovieApp = () => {
                   </Typography>
                   <Button
                     variant="contained"
-                    color="secondary"
+                    color='info'
                     onClick={() => window.open(`https://www.themoviedb.org/movie/${movie.id}`)}
-                    sx={{ marginRight: 2 }}
+                    sx={{ marginRight: 2  }}
                   >
                     View on TMDB
                   </Button>
@@ -126,6 +141,15 @@ const MovieApp = () => {
             </Grid>
           ))}
       </Grid>
+      <div>
+        <Button onClick={() => handlePagination(currentPage - 1)} disabled={currentPage === 1}>
+          Previous Page
+        </Button>
+        <span>{`Page ${currentPage} of ${totalPages}`}</span>
+        <Button onClick={() => handlePagination(currentPage + 1)} disabled={currentPage === totalPages}>
+          Next Page
+        </Button>
+      </div>
     </Container>
   );
 };
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,17 +4,17 @@ import SlideshowIcon from '@mui/icons-material/Slideshow';
 
 const MovieHeader = () => {
   return (
-    <AppBar position="sticky" color="inherit" sx={{ backgroundColor: 'orange' }}>
+    <AppBar position="sticky" color="inherit" sx={{ backgroundColor: 'black' }}>
       <Toolbar>
         <SlideshowIcon 
         color='success'
         fontSize="large"/>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 3, color: 'black',fontWeight:900 ,fontSize:"xlarge", paddingLeft:2}}>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 3, color: 'white',fontWeight:900 ,fontSize:"xlarge", paddingLeft:2}}>
           Movie Site
         </Typography>
-        <Button color="primary" sx={{ color: 'black',fontWeight:600 }}>Home</Button>
-        <Button color="primary" sx={{ color: 'black',fontWeight:600 }}>Movies</Button>
-        <Button color="primary" sx={{ color: 'black',fontWeight:600 }}>Sign In</Button>
+        <Button color="primary" sx={{ color: 'white',fontWeight:600 }}>Home</Button>
+        <Button color="primary" sx={{ color: 'white',fontWeight:600 }}>Movies</Button>
+        <Button color="primary" sx={{ color: 'white',fontWeight:600 }}>Sign In</Button>
       </Toolbar>
     </AppBar>
   );
